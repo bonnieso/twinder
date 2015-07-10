a7e59a7c7f4d33480359f0482849095febf99384
bonnieso <so_bonnie@hotmail.com>
2015-07-09 19:53:41 -0700

    adding twinder

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,11 @@
 // do the magic
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+ return gulp.src('./source/javascripts/**/*.js')
+   .pipe(concat('all.js'))
+   .pipe(gulp.dest('./public/javascripts/'));
+})
+
+gulp.task('default', ['scripts']);
